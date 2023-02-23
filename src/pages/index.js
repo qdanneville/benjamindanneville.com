a4de1a50153ed2ab8d536b175e6aa51a113143a7
qdanneville <quentin@esperluette.io>
2023-02-23 11:21:33 +0100

    [Config] Content from .md

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
+import { getAllPosts } from '../lib/api';
 
-export default function Home() {
+export default function Home({ allPosts }) {
+  
+  const heroPost = allPosts[0]
+  
+  console.log(heroPost);
+
   return (
     <>
       <Head>
@@ -9,11 +15,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="flex items-center justify-center w-screen h-screen bg-gray-900">
+      <main className='flex items-center justify-center w-screen h-screen bg-gray-900'>
+        <div className="flex items-center w-1/3">
           <article className='text-white pr-8'>
-            <h1 className='text-2xl font-medium underline'>Benjamin Danneville</h1>
-            <h2 className='text-1xl font-light'>Animation and VFX Industry Pipeline TD.</h2>
+            <h1 className='text-2xl font-medium underline'>{heroPost.title}</h1>
+            <h2 className='text-1xl font-light'>{heroPost.excerpt}</h2>
           </article>
           <aside>
             <div className="w-40 h-40 rounded-full bg-gray-800"></div>
@@ -22,4 +28,15 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
 }
